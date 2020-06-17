df0a495f7af8c48dbedae3710bdb9026541af46e
YuriU <jerryhire8@gmail.com>
2020-06-17 22:22:45 +0300

    Basic permutation print

---
+++ NextPermutation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NextPermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if(j == i)
+                        continue;
+                    
+                    for (int k = 0; k < array.Length; k++)
+                    {
+                        if(k == j || k == i)
+                            continue;
+                        
+                        Console.WriteLine($"{array[i]} {array[j]} {array[k]}");
+                    }
+                }
+            }
+        }
+    }
+}
